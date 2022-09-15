43de0dcdba0b39d22f170ad21b13d0b770c079b2
Behchet Behchetov <behchet@dsi.bg>
2022-09-15 10:26:56 +0300

    core refactored;

--- src/jobs/cron/get-keywords.cron.ts
+++ src/jobs/cron/get-keywords.cron.ts
@@ -7,7 +7,7 @@ export function getKeywords() {
   console.log(`[${JOB_NAME}] Will start at 10:00AM on Wednesday...`);
   
   return cron.schedule(
-    '*/20 * * * * *',
+    '* */20 * * * *',
     () => {
       console.log(`[${JOB_NAME}] Started...`);
       const gKeywordService = new GoogleKeywordService();
