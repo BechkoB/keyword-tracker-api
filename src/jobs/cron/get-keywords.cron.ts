c125218ecb51fc520a071d373f2e3e76b1a97c99
Behchet Behchetov <behchet@dsi.bg>
2023-01-26 12:22:33 +0200

    cronjob scheduled;

--- src/jobs/cron/get-keywords.cron.ts
+++ src/jobs/cron/get-keywords.cron.ts
@@ -1,24 +1,25 @@
-import * as cron  from 'node-cron';
-import { GoogleKeywordService } from '../../services/google-keyword.service';
+import * as cron from "node-cron";
+import { GoogleKeywordService } from "../../services/google-keyword.service";
 
-const JOB_NAME = 'JOB:GET_GOOGLE_KEYWORDS';
+const JOB_NAME = "JOB:GET_GOOGLE_KEYWORDS";
 
 export function getKeywords() {
   console.log(`[${JOB_NAME}] Will start at 10:00AM on Wednesday...`);
-  
+
   // setTimeout(() => {
-  //   const gKeywordService = new GoogleKeywordService();
-  //   return gKeywordService.fetchAllData();
+  //
   // }, 120000);
 
-  // return cron.schedule(
-  //   '00 20 14 * * 2',
-  //   () => {
-  //     console.log(`[${JOB_NAME}] Started...`);
-  //   },
-  //   {
-  //     scheduled: true,
-  //     timezone: 'Europe/Sofia'
-  //   }
-  // );
+  return cron.schedule(
+    "00 25 12 * * 4",
+    () => {
+      console.log(`[${JOB_NAME}] Started...`);
+      const gKeywordService = new GoogleKeywordService();
+      return gKeywordService.fetchAllData();
+    },
+    {
+      scheduled: true,
+      timezone: "Europe/Sofia",
+    }
+  );
 }
