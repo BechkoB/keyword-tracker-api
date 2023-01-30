961938dc19f3ade9df16a02aab894cecef20caa3
Behchet Behchetov <behchet@dsi.bg>
2023-01-30 09:54:27 +0200

    cron time edited;

--- src/jobs/cron/get-keywords.cron.ts
+++ src/jobs/cron/get-keywords.cron.ts
@@ -10,7 +10,7 @@ export function getKeywords() {
   // }, 120000);
   
   cron.schedule(
-    "00 25 12 * * 4",
+    "00 00 10 * * 1",
     () => {
       const gKeywordService = new GoogleKeywordService();
       gKeywordService.fetchAllData();
