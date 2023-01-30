da18cbafec7895de7d6dd34da71467edef491ded
Behchet Behchetov <behchet@dsi.bg>
2023-01-30 11:55:15 +0200

    cron schedule dates updated;

--- src/services/google-keyword.service.ts
+++ src/services/google-keyword.service.ts
@@ -13,8 +13,8 @@ export class GoogleKeywordService {
   async fetchAllData() {
     // const endDate = moment().subtract(3, "days").format("YYYY-MM-DD");
     // const startDate = moment(endDate).subtract(7, "days").format("YYYY-MM-DD");
-    const endDate = "2023-01-01";
-    const startDate = "2022-12-26";
+    const endDate = "2023-01-08";
+    const startDate = "2023-01-02";
     console.log(
       "Fetching keywords from Google started..." +
         moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
@@ -37,7 +37,7 @@ export class GoogleKeywordService {
         from === "query" ? x.keys[0] : x.keys[1].replace(MAIN_URL, "");
       x["date_start"] = startDate;
       x["date_end"] = endDate;
-      x["created_at"] = new Date("2023-01-04");
+      x["created_at"] = new Date("2023-01-11");
     });
 
     if (from === "query") {
