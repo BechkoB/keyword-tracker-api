478984bfdd5a192288f85c0786682aa04ef4e01e
Behchet Behchetov <behchet@dsi.bg>
2023-02-07 10:05:44 +0200

    cron schedule updated;

--- src/jobs/cron/get-keywords.cron.ts
+++ src/jobs/cron/get-keywords.cron.ts
@@ -5,23 +5,23 @@ const JOB_NAME = "JOB:GET_GOOGLE_KEYWORDS";
 
 export function getKeywords() {
   console.log(`[${JOB_NAME}] Will start at 10:00AM on Wednesday...`);
-
-  // setTimeout(() => {
-  //   startJob();
-  // }, 120000);
-}
-
-function startJob() {
-  return cron.schedule(
-    "00 20 10 * * 2",
-    () => {
-      const gKeywordService = new GoogleKeywordService();
-      gKeywordService.fetchAllData();
+  cron.schedule(
+    "00 10 10 * * 2",
+    async () => {
       console.log(`[${JOB_NAME}] Started...`);
+      const gKeywordService = new GoogleKeywordService();
+      await gKeywordService.fetchAllData();
     },
     {
       scheduled: true,
       timezone: "Europe/Sofia",
     }
   );
+  // setTimeout(() => {
+  //   startJob();
+  // }, 120000);
+}
+
+function startJob() {
+  return;
 }
--- src/services/google-keyword.service.ts
+++ src/services/google-keyword.service.ts
@@ -13,8 +13,8 @@ export class GoogleKeywordService {
   async fetchAllData() {
     // const endDate = moment().subtract(3, "days").format("YYYY-MM-DD");
     // const startDate = moment(endDate).subtract(7, "days").format("YYYY-MM-DD");
-    const endDate = "2023-01-22";
-    const startDate = "2023-01-16";
+    const endDate = "2023-01-29";
+    const startDate = "2023-01-23";
     console.log(
       "Fetching keywords from Google started..." +
         moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
@@ -37,7 +37,7 @@ export class GoogleKeywordService {
         from === "query" ? x.keys[0] : x.keys[1].replace(MAIN_URL, "");
       x["date_start"] = startDate;
       x["date_end"] = endDate;
-      x["created_at"] = new Date("2023-01-25");
+      x["created_at"] = new Date("2023-02-01");
     });
 
     if (from === "query") {
