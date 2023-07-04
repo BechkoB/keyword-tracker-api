72424bbbd68f30afb94cbf6e7bd63ec5f4052bc6
Behchet Behchetov <behchet@dsi.bg>
2023-07-04 14:31:10 +0300

    dates updated;

--- src/jobs/cron/get-keywords.cron.ts
+++ src/jobs/cron/get-keywords.cron.ts
@@ -13,33 +13,33 @@ export function getKeywords() {
   //   return gKeywordService.fetchAllData();
   // }, 120000);
 
-  // return cron.schedule(
-  //   '00 15 09 * * 3',
-  //   () => {
-  //     const memoryData = process.memoryUsage()
+  return cron.schedule(
+    '00 14 40 * * 2',
+    () => {
+      const memoryData = process.memoryUsage()
 
-  //     const memoryUsage = {
-  //       rss: `${formatMemoryUsage(
-  //         memoryData.rss
-  //       )} -> Resident Set Size - total memory allocated for the process execution`,
-  //       heapTotal: `${formatMemoryUsage(
-  //         memoryData.heapTotal
-  //       )} -> total size of the allocated heap`,
-  //       heapUsed: `${formatMemoryUsage(
-  //         memoryData.heapUsed
-  //       )} -> actual memory used during the execution`,
-  //       external: `${formatMemoryUsage(
-  //         memoryData.external
-  //       )} -> V8 external memory`,
-  //     }
-  //     console.log(memoryUsage, 'before job')
-  //     console.log(`[${JOB_NAME}] Started...`)
-  //     const gKeywordService = new GoogleKeywordService()
-  //     return gKeywordService.fetchAllData()
-  //   },
-  //   {
-  //     scheduled: true,
-  //     timezone: 'Europe/Sofia',
-  //   }
-  // )
+      const memoryUsage = {
+        rss: `${formatMemoryUsage(
+          memoryData.rss
+        )} -> Resident Set Size - total memory allocated for the process execution`,
+        heapTotal: `${formatMemoryUsage(
+          memoryData.heapTotal
+        )} -> total size of the allocated heap`,
+        heapUsed: `${formatMemoryUsage(
+          memoryData.heapUsed
+        )} -> actual memory used during the execution`,
+        external: `${formatMemoryUsage(
+          memoryData.external
+        )} -> V8 external memory`,
+      }
+      console.log(memoryUsage, 'before job')
+      console.log(`[${JOB_NAME}] Started...`)
+      const gKeywordService = new GoogleKeywordService()
+      return gKeywordService.fetchAllData()
+    },
+    {
+      scheduled: true,
+      timezone: 'Europe/Sofia',
+    }
+  )
 }
