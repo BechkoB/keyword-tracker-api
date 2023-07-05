10d5bd85a52e334f2ccedac0382a23bd9c9808b9
Behchet Behchetov <behchet@dsi.bg>
2023-07-05 12:09:37 +0300

    dates changed;

--- src/jobs/cron/get-keywords.cron.ts
+++ src/jobs/cron/get-keywords.cron.ts
@@ -14,7 +14,7 @@ export function getKeywords() {
   // }, 120000);
 
   return cron.schedule(
-    '00 40 08 * * 3',
+    '00 20 12 * * 3',
     () => {
       const memoryData = process.memoryUsage()
 
