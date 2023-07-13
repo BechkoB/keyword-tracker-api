30f4cf8017fbd3459d2da0e8607bcc3160b63338
Behchet Behchetov <behchet@dsi.bg>
2023-07-13 15:45:46 +0300

    console.log removed;

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,16 +1,20 @@
-import axios from 'axios'
-import dotenv = require('dotenv')
-dotenv.config()
-restartDyno()
+import axios from 'axios';
+import dotenv = require('dotenv');
+import * as moment from 'moment';
+dotenv.config();
+restartDyno();
 
 async function restartDyno() {
-  console.log('Dyno restarted')
-  const token = process.env.HEROKU_TOKEN
-  console.log(token)
-  await axios.delete('https://api.heroku.com/apps/gkeyword-api/dynos', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: `application/vnd.heroku+json; version=3`,
-    },
-  })
+  console.log(
+    `Dyno restarted... At: ${moment().format('dddd, MMMM Do YYYY, h:mm:ss a')}`
+  )
+  await axios.delete(
+    'https://api.heroku.com/apps/gkeyword-api/dynos',
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HEROKU_TOKEN}`,
+        Accept: `application/vnd.heroku+json; version=3`,
+      },
+    }
+  )
 }
