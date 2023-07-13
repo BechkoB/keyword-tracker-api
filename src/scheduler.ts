3ca96b1b3de9f55e060aba662e7a0789751d58a4
Behchet Behchetov <behchet@dsi.bg>
2023-07-13 15:32:13 +0300

    type removed from package.json

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,12 +1,16 @@
 import axios from 'axios'
+import dotenv = require('dotenv')
+dotenv.config()
 restartDyno()
 
 async function restartDyno() {
   console.log('Dyno restarted')
   const token = process.env.HEROKU_TOKEN
+  console.log(token)
   await axios.delete('https://api.heroku.com/apps/gkeyword-api/dynos', {
     headers: {
       Authorization: `Bearer ${token}`,
+      Accept: `application/vnd.heroku+json; version=3`,
     },
   })
 }
